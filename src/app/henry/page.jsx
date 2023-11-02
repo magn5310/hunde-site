a60c7a2457c69a47b6c91714566a4106d376575e
magnus madsen <magn@stud.kea.dk>
2023-11-02 09:54:08 +0100

    henry

---
+++ src/app/henry/page.jsx
@@ -0,0 +1,18 @@
+async function page() {
+  const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
+  const data = await res.json();
+  const { name, favouriteColor, age, image } = data;
+  console.log(data);
+  return (
+    <div>
+      <main>
+        <h1>{name}</h1>
+        <h2>{favouriteColor}</h2>
+        <h3>{age}</h3>
+        <img className="w-96" src={image.url} alt="" />
+      </main>
+    </div>
+  );
+}
+
+export default page;
